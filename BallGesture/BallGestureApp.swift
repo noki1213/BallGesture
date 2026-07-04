dc64c597710d755b0f655c71d949f43ff6e53472
noki1213 <208437777+noki1213@users.noreply.github.com>
2026-07-04 16:55:14 +0900

    Initial Commit

---
+++ BallGesture/BallGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallGestureApp.swift
+//  BallGesture
+//
+//  Created by noki1213 on 2026/07/04.
+//
+
+import SwiftUI
+
+@main
+struct BallGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
